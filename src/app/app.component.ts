34e96e45bf41400a0ad67542507ecbb6e2814571
Zgrot <denoirjean.thomas@gmail.com>
2024-10-17 10:12:30 +0200

    Implémentation de l'affichage soit pokemon unique soit liste de pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { PokemonCard } from './app.components.pokemoncard';
@@ -8,7 +9,7 @@ import { TeamPokemonCard } from './app.component.teampokemoncard';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PokemonCard, PokemonList, TeamPokemonCard, FormsModule],
+  imports: [RouterOutlet, PokemonCard, PokemonList, TeamPokemonCard, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,6 +19,7 @@ export class AppComponent {
   getPokemonByRandom: boolean = false;
   nameOrPokedexNumber: string | number = ''
   pokemonsToDisplayUrls: string[] = []
+  displayMode: string = ''
 
   private NUMBER_OF_POKEMON_TO_DISPLAY: number = 6;
 
