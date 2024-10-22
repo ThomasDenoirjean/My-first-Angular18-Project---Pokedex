a9de0f55f4d516963f4cf2434ef068fbb10139d6
Zgrot <denoirjean.thomas@gmail.com>
2024-10-22 15:00:28 +0200

    Fix du composant pokemonlist - renvoi bien maintenant le nom et l'url du front_default sprite

--- src/app/teamselection/services/fetchpokemonsbytype.service.ts
+++ src/app/teamselection/services/fetchpokemonsbytype.service.ts
@@ -5,10 +5,13 @@ import { Pokemon } from '../../pokemon.interface';
 import { FetchPokemonFrontDefaultSpriteService } from './fetchpokemonfrontdefaultsprite.service';
 
 export interface PokemonTypeResponse {
+    pokemon: {
         pokemon: {
             name: string;
             url: string;
-        }[];
+        };
+        slot: number;
+    }[];
 }
 
 @Injectable({
@@ -23,27 +26,12 @@ export class FetchPokemonsByTypeService {
     getPokemonsByType(type: string): Observable<Pokemon[]> {
         let apiUrl = `https://pokeapi.co/api/v2/type/${type}/`;
 
-
-        this.http.get<PokemonTypeResponse>(apiUrl).subscribe(
-            (response) => {
-              // Ici, 'response' contient les données récupérées depuis l'API
-              const pokemonName = response.pokemon[0].name;
-              console.log('pokemonName', pokemonName);
-            },
-            (error) => {
-              // Gestion des erreurs éventuelles
-              console.error('Erreur lors de la récupération des données :', error);
-            }
-        );
-
         return this.http.get<PokemonTypeResponse>(apiUrl).pipe(
             mergeMap(response => {
-                // console.log('response in fetchpokemonbytype', response.pokemon) //.pokemonSlot[0].pokemon.name
-
-                const pokemonObservables = response.pokemon.map(pokemon =>
-                    this.fetchPokemonFrontDefaultSpriteService.getPokemonFrontDefaultSprite(pokemon.name).pipe(
+                const pokemonObservables = response.pokemon.map(pokemonEntry =>
+                    this.fetchPokemonFrontDefaultSpriteService.getPokemonFrontDefaultSprite(pokemonEntry.pokemon.name).pipe(
                         map(spriteUrl => ({
-                            name: pokemon.name,
+                            name: pokemonEntry.pokemon.name,
                             sprites: { front_default: spriteUrl }
                         } as Pokemon))
                     )
